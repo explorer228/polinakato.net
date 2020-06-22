67225b9047e951f53cc75f77e691276cb3b917ca
explorer228 <sendtopolina@gmail.com>
2020-06-22 19:10:43 -0400

    js

---
+++ script.js
@@ -0,0 +1,21 @@
+const randomNumber = Math.floor(Math.random()*7)
+
+const video = document.getElementById("dust");
+video.style.zIndex = randomNumber;
+console.log("dust Z-INDEX: ", randomNumber)
+
+const video = document.getElementById("abstract");
+video.style.zIndex = randomNumber;
+console.log("abstract Z-INDEX: ", randomNumber)
+
+const video = document.getElementById("stream");
+video.style.zIndex = randomNumber;
+console.log("stream Z-INDEX: ", randomNumber)
+
+const video = document.getElementById("forest");
+video.style.zIndex = randomNumber;
+console.log("forest Z-INDEX: ", randomNumber)
+
+const video = document.getElementById("sun");
+video.style.zIndex = randomNumber;
+console.log("sun Z-INDEX: ", randomNumber)
